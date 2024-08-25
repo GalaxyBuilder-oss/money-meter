c0fb34e0b66a599da29c68346bdc9e963ab7800a
GalaxyBuilder-oss <galaxybuilder.oss@gmail.com>
2024-08-26 00:01:38 +0700

    Initialization Backend: Start Backend

---
+++ backend/src/main/java/com/gb/moneymeter/MoneyMeterApplication.java
@@ -0,0 +1,13 @@
+package com.gb.moneymeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMeterApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gb/moneymeter/MoneyMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gb.moneymeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
